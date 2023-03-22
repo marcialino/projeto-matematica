e4977297c320a06ce8f87484ffaae358ad52c6d8
marcialino <marcialino100@gmail.com>
2023-03-22 15:23:27 -0300

    Criei projeto-matematica

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert('Olá!')
+function calcular(){
+    var alt = window.document.getElementById('alt')
+    var bas = window.document.getElementById('bas')
+    var res = window.document.getElementById('res')
+
+    var a = Number(alt.value)
+    var b = Number(bas.value)
+
+    var area = a*b/2
+
+    res.innerHTML += `<p style="font:bold 12pt Arial;"> A área do triângulo com altura ${a} e base ${b} é de ${area}</p>`
+
+}
+
+function calcular1(){
+    var raio = window.document.getElementById('raio')
+    
+}
